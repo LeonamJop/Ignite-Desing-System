8b0f6dbf2781a54488ff5fa9a1ede9dfd34e8eb4
Leonam Silva <leonam.murilo@gmail.com>
2023-08-07 17:18:24 -0300

    feature: story: button

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,8 +1,8 @@
-/// <reference type="vite/client"/>
+// <reference type="vite/client"/>
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [react()]
+  plugins: [react()],
 })
