9eb3160fc3455211d4deb744d0cdae884b1a4a2c
Leonam Silva <leonam@nexti.com>
2023-09-14 22:51:53 -0300

    feat: created avatar component

---
+++ packages/docs/src/stories/Avata.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://github.com/leonamjop.png',
+    alt: 'Leonam Silva',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
