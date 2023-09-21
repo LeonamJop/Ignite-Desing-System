3166a02185fba0ba1b00050de6943208e009cf2b
Leonam Silva <leonam.murilo@gmail.com>
2023-09-21 20:21:28 -0300

    docs: added components controllers

--- packages/docs/src/stories/Avata.stories.tsx
+++ packages/docs/src/stories/Avata.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/leonamjop.png',
     alt: 'Leonam Silva',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
